19e3f4b1d316d2c5d24c44807e9a64fe8bfe10de
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-07-13 20:37:38 +0200

    Use uber multierr for now

--- errors_multi.go
+++ errors_multi.go
@@ -1,91 +1,8 @@
 package errors
 
-// multiError aggregates multiple errors into a single value.
-// Also implements the error interface so it can be returned as an error.
-type multiError struct {
-	errors []error
-	msg    string
-}
-
-// Error implements the error interface.
-func (e *multiError) Error() string {
-	if e == nil {
-		return ""
-	}
-
-	if e.msg != "" {
-		return e.msg
-	}
-
-	return "multiple errors happened"
-}
-
-// Errors returns the list of wrapped errors.
-func (e *multiError) Errors() []error {
-	if e == nil {
-		return nil
-	}
-
-	return e.errors
-}
-
-// createMultiError aggregates the given list of errors into a single error in an efficient way.
-// Optimizations include:
-// 	- flattening multiErrors
-// 	- calculating the necessary size for the error slice
-// 	- skipping nil errors
-func createMultiError(errors []error) error {
-	first := true
-	var count, firstIdx int
-	isFlat := true
-
-	for i, err := range errors {
-		if err == nil {
-			continue
-		}
-
-		count++
-		if first {
-			first = false
-			firstIdx = i
-		}
-
-		if m, ok := err.(*multiError); ok {
-			count += len(m.errors) - 1 // the error itself is already counted once
-			isFlat = false
-		}
-	}
-
-	// no errors, return nil
-	if count == 0 {
-		return nil
-	}
-
-	// there is one error, identified by firstIdx, return it
-	if count == 1 && isFlat {
-		return errors[firstIdx]
-	}
-
-	if count == len(errors) && isFlat {
-		// TODO: copy the errors?
-		return &multiError{errors: errors}
-	}
-
-	errs := make([]error, 0, count)
-	for _, err := range errors[firstIdx:] {
-		if err == nil {
-			continue
-		}
-
-		if m, ok := err.(*multiError); ok {
-			errs = append(errs, m.errors...)
-		} else {
-			errs = append(errs, err)
-		}
-	}
-
-	return &multiError{errors: errs}
-}
+import (
+	"go.uber.org/multierr"
+)
 
 // Combine combines the passed errors into a single error.
 //
@@ -113,7 +30,26 @@ func createMultiError(errors []error) error {
 // The returned error formats into a readable multi-line error message if
 // formatted with %+v.
 //
-// 	fmt.Sprintf("%+v", multierr.Combine(err1, err2))
+// 	fmt.Sprintf("%+v", errors.Combine(err1, err2))
 func Combine(errors ...error) error {
-	return createMultiError(errors)
+	return multierr.Combine(errors...)
+}
+
+// Append appends the given errors together. Either value may be nil.
+//
+// This function is a specialization of Combine for the common case where
+// there are only two errors.
+//
+// 	err = errors.Append(reader.Close(), writer.Close())
+//
+// The following pattern may also be used to record failure of deferred
+// operations without losing information about the original error.
+//
+// 	func doSomething(..) (err error) {
+// 		f := acquireResource()
+// 		defer func() {
+// 			err = errors.Append(err, f.Close())
+// 		}()
+func Append(left error, right error) error {
+	return multierr.Append(left, right)
 }
